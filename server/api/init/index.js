b152b0e41e809d3177eaaf4b462d573d31dd723e
Ishaan Puniani <ishaan.puniani.ext@netent.com>
2016-04-26 10:03:36 +0200

    Updated Response

--- server/api/init/index.js
+++ server/api/init/index.js
@@ -2,8 +2,12 @@
 var reelset = require('../../config/reelset');
 var betlines = require('../../config/betlines');
 
-function init() {
+function init(game) {
+
     return {
+        roundOver:true,
+        bet:0,
+        win:0,
         symbols: reelset.initial,
         betlines:betlines.betlines
     }
--- server/api/spin/index.js
+++ server/api/spin/index.js
@@ -5,17 +5,20 @@ var generator = new MersenneTwister();
 
 var reelset = require('../../config/reelset');
 
-function spin() {
+function spin(game) {
     //   var target = Math.floor(generator.random() * 10);
     var reelset_0  = reelset.reels[0];
     var reelset_1  = reelset.reels[1];
     var reelset_2  = reelset.reels[2];
     
     return {
+        roundOver:true,
+        bet:game.bet,
+        win:game.bet * 2,
         symbols: [
             reelset_0[Math.floor(generator.random() * reelset_0.length)], 
-            reelset_1[Math.floor(generator.random() * reelset_1.length)],, 
-            reelset_2[Math.floor(generator.random() * reelset_2.length)],
+            reelset_1[Math.floor(generator.random() * reelset_1.length)], 
+            reelset_2[Math.floor(generator.random() * reelset_2.length)]
         ]
     }
 }
